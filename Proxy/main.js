5cdcf53838c4761adadbd0893fa13b81b1085261
Akshit Patel <aapatel8@ncsu.edu>
2018-05-06 22:08:14 -0400

    Updated proxy

--- Proxy/main.js
+++ Proxy/main.js
@@ -1,69 +1,63 @@
 var http = require('http'),
-    httpProxy = require('http-proxy');
-var url = require( "url"); 
+  httpProxy = require('http-proxy');
+var url = require("url");
 var proxy = httpProxy.createProxyServer({});
-var redis = require('redis')
 
 var ip;
-var flag = 0;
-var timer = 0;
+var isBot = 0;
+var expirationTimer = 0;
 var maxRequestCount = 2;
-var blacklisted = {};
-var count = {};
+var ipCount = {};
+
+function determineIfBot() {
+  if (!ipCount[ip]) {
+    ipCount[ip] = 1;
+    console.log(ipCount[ip]);
+  } else {
+    ipCount[ip] += 1;
+  }
+
+  console.log("IP address: " + ip + "\tRequest ipCount: " + ipCount[ip]);
+  if (ipCount[ip] > maxRequestCount) {
+    isBot = 1;
+  }
+}
 
 var server = http.createServer(function(req, res) {
 
- setInterval(function(){
- timer++;
- if(timer == 10)
-   timer = 0;
-}, 1*1000);
+  setInterval(function() {
+    expirationTimer++;
+    //Reset ipCount every 15 seconds
+    if (expirationTimer == 15)
+      expirationTimer = 0;
+  }, 1000);
 
-if( timer == 0)
-{
- count = {};
-}
-
-ip = req.connection.remoteAddress;
-
-if (ip.substr(0, 7) == "::ffff:") {
-  ip = ip.substr(7)
-}
-
-calculate();
-
-if (flag == 0) {
-     console.log("Redirected to the main Checkbox.io server")
-     targetUrl = 'http://138.197.73.154';
+  if (expirationTimer == 0) {
+    ipCount = {};
   }
-  else {
-     console.log("Redirected to our police server")
-     targetUrl = 'http://159.65.174.84:8080';
+
+  ip = req.connection.remoteAddress;
+
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
   }
-  
-proxy.web(req, res, { target: targetUrl });
-  flag = 0;
+
+  determineIfBot();
+
+  if (isBot == 0) {
+    console.log("Redirected to the main Checkbox.io droplet")
+    targetUrl = 'http://138.197.73.154';
+  } else {
+    console.log("Redirected to the Police monkey droplet")
+    targetUrl = 'http://159.65.174.84:8080';
+  }
+
+  proxy.web(req, res, {
+    target: targetUrl
+  });
+  isBot = 0;
 });
 
 console.log("Proxy server listening on port 8080");
 
 server.listen(8080);
-
-function calculate()
-{
- if(!count[ip] )
- {
-    count[ip] = 1;
-    console.log(count[ip]);
- }
-  else{
-      count[ip] += 1;
- }
-
- console.log("IP address: " + ip+ "\tRequest count: " + count[ip]);
-  if(count[ip] > maxRequestCount || blacklisted[ip] == 1 )
-  {
-        blacklisted[ip] = 1;
-        flag = 1;
-  }
-}
